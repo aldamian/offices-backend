269f2822f135d94241402f8022e20a4549bc0c37
Alexandru Damian <DamianAlexandru.A@gmail.com>
2022-02-28 23:29:37 +0100

    update

--- offices/settings.py
+++ offices/settings.py
@@ -7,13 +7,8 @@ from decouple import config
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = ['offices-backend.herokuapp.com', '127.0.0.1']
